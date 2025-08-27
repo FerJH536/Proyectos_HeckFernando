ef82b6239a121be9981a1a7ad188c2abee8ec0e2
FerJH536 <fer536210@gmail.com>
2025-08-27 17:46:21 -0300

    cambio de prueba

--- index2.php
+++ index2.php
@@ -3,7 +3,7 @@
 <head>
     <link rel="stylesheet" href="estiloarray.css">
     <meta charset="UTF-8">
-    <title>Reporte de Estudiantes</title>
+    <title>Reporte de Estudiantess</title>
 </head>
 <body>
 
